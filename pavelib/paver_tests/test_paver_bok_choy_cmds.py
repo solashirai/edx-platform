f85901abb3ebe08a390693e29928f59ff5bad907
Ben Patterson <bpatterson@edx.org>
2014-09-05 20:45:22 -0400

    Additional tests of the paver test_bokchoy command.

--- pavelib/paver_tests/test_paver_bok_choy_cmds.py
+++ pavelib/paver_tests/test_paver_bok_choy_cmds.py
@@ -9,13 +9,50 @@ REPO_DIR = os.getcwd()
 class TestPaverBokChoy(unittest.TestCase):
 
     def test_default_bokchoy(self):
-        command = BokChoyTestSuite('paver -t test_bokchoy')
-        expected_output = ("SCREENSHOT_DIR='{repo_dir}/test_root/log' "
+        request = BokChoyTestSuite('paver test_bokchoy')
+        expected_command = ("SCREENSHOT_DIR='{repo_dir}/test_root/log' "
                                        "HAR_DIR='{repo_dir}/test_root/log/hars' "
                                        "SELENIUM_DRIVER_LOG_DIR='{repo_dir}/test_root/log' "
                                        "nosetests {repo_dir}/common/test/acceptance/tests "
                                        "--with-xunit "
                                        "--xunit-file={repo_dir}/reports/bok_choy/xunit.xml "
                                        "--verbosity=2 ".format(repo_dir=REPO_DIR))
-        print expected_output
-        self.assertTrue(command.cmd == expected_output)
+        self.assertEqual(request.cmd, expected_command)
+
+    def test_suite_request_bokchoy(self):
+        request = BokChoyTestSuite('paver test_bokchoy')
+        request.test_spec = "test_foo.py"
+        expected_command = ("SCREENSHOT_DIR='{repo_dir}/test_root/log' "
+                                       "HAR_DIR='{repo_dir}/test_root/log/hars' "
+                                       "SELENIUM_DRIVER_LOG_DIR='{repo_dir}/test_root/log' "
+                                       "nosetests {repo_dir}/common/test/acceptance/tests/test_foo.py "
+                                       "--with-xunit "
+                                       "--xunit-file={repo_dir}/reports/bok_choy/xunit.xml "
+                                       "--verbosity=2 ".format(repo_dir=REPO_DIR))
+        self.assertEqual(request.cmd, expected_command)
+
+    def test_class_request_bokchoy(self):
+        request = BokChoyTestSuite('paver test_bokchoy')
+        request.test_spec = "test_foo.py:FooTest"
+        expected_command = ("SCREENSHOT_DIR='{repo_dir}/test_root/log' "
+                                       "HAR_DIR='{repo_dir}/test_root/log/hars' "
+                                       "SELENIUM_DRIVER_LOG_DIR='{repo_dir}/test_root/log' nosetests "
+                                       "{repo_dir}/common/test/acceptance/tests/test_foo.py:FooTest "
+                                       "--with-xunit "
+                                       "--xunit-file={repo_dir}/reports/bok_choy/xunit.xml "
+                                       "--verbosity=2 ".format(repo_dir=REPO_DIR))
+        self.assertEqual(request.cmd, expected_command)
+
+    def test_case_request_bokchoy(self):
+        request = BokChoyTestSuite('paver test_bokchoy')
+        request.test_spec = "test_foo.py:FooTest.test_bar"
+        expected_command = ("SCREENSHOT_DIR='{repo_dir}/test_root/log' "
+                                       "HAR_DIR='{repo_dir}/test_root/log/hars' "
+                                       "SELENIUM_DRIVER_LOG_DIR='{repo_dir}/test_root/log' nosetests "
+                                       "{repo_dir}/common/test/acceptance/tests/test_foo.py:FooTest.test_bar "
+                                       "--with-xunit "
+                                       "--xunit-file={repo_dir}/reports/bok_choy/xunit.xml "
+                                       "--verbosity=2 ".format(repo_dir=REPO_DIR))
+        self.assertEqual(request.cmd, expected_command)
+
+    # TODO: Test when bok_choy test file is in a subdir
