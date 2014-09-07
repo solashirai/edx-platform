de041d5680643279f13dfbf56661c8776f2bd720
Ben Patterson <bpatterson@edx.org>
2014-09-06 20:48:46 -0400

    Refactor paver_tests.

--- pavelib/paver_tests/test_paver_bok_choy_cmds.py
+++ pavelib/paver_tests/test_paver_bok_choy_cmds.py
@@ -8,51 +8,36 @@ REPO_DIR = os.getcwd()
 
 class TestPaverBokChoy(unittest.TestCase):
 
-    def test_default_bokchoy(self):
-        request = BokChoyTestSuite('paver test_bokchoy')
-        expected_command = ("SCREENSHOT_DIR='{repo_dir}/test_root/log' "
+    def setUp(self):
+        self.request = BokChoyTestSuite('')
+
+    def _expected_command(self, expected_text_append):
+        if expected_text_append:
+            expected_text_append = "/" + expected_text_append
+        expected_statement = ("SCREENSHOT_DIR='{repo_dir}/test_root/log' "
                                        "HAR_DIR='{repo_dir}/test_root/log/hars' "
                                        "SELENIUM_DRIVER_LOG_DIR='{repo_dir}/test_root/log' "
-                                       "nosetests {repo_dir}/common/test/acceptance/tests "
+                                       "nosetests {repo_dir}/common/test/acceptance/tests{exp_text} "
                                        "--with-xunit "
                                        "--xunit-file={repo_dir}/reports/bok_choy/xunit.xml "
-                                       "--verbosity=2 ".format(repo_dir=REPO_DIR))
-        self.assertEqual(request.cmd, expected_command)
+                                       "--verbosity=2 ".format(repo_dir=REPO_DIR,
+                                                               exp_text=expected_text_append))
+        return expected_statement
+
+    def test_default_bokchoy(self):
+        self.assertEqual(self.request.cmd, self._expected_command(''))
 
     def test_suite_request_bokchoy(self):
-        request = BokChoyTestSuite('paver test_bokchoy')
-        request.test_spec = "test_foo.py"
-        expected_command = ("SCREENSHOT_DIR='{repo_dir}/test_root/log' "
-                                       "HAR_DIR='{repo_dir}/test_root/log/hars' "
-                                       "SELENIUM_DRIVER_LOG_DIR='{repo_dir}/test_root/log' "
-                                       "nosetests {repo_dir}/common/test/acceptance/tests/test_foo.py "
-                                       "--with-xunit "
-                                       "--xunit-file={repo_dir}/reports/bok_choy/xunit.xml "
-                                       "--verbosity=2 ".format(repo_dir=REPO_DIR))
-        self.assertEqual(request.cmd, expected_command)
+        self.request.test_spec = "test_foo.py"
+        self.assertEqual(self.request.cmd, self._expected_command(self.request.test_spec))
 
     def test_class_request_bokchoy(self):
-        request = BokChoyTestSuite('paver test_bokchoy')
-        request.test_spec = "test_foo.py:FooTest"
-        expected_command = ("SCREENSHOT_DIR='{repo_dir}/test_root/log' "
-                                       "HAR_DIR='{repo_dir}/test_root/log/hars' "
-                                       "SELENIUM_DRIVER_LOG_DIR='{repo_dir}/test_root/log' nosetests "
-                                       "{repo_dir}/common/test/acceptance/tests/test_foo.py:FooTest "
-                                       "--with-xunit "
-                                       "--xunit-file={repo_dir}/reports/bok_choy/xunit.xml "
-                                       "--verbosity=2 ".format(repo_dir=REPO_DIR))
-        self.assertEqual(request.cmd, expected_command)
+        self.request.test_spec = "test_foo.py:FooTest"
+        self.assertEqual(self.request.cmd, self._expected_command(self.request.test_spec))
 
     def test_case_request_bokchoy(self):
-        request = BokChoyTestSuite('paver test_bokchoy')
-        request.test_spec = "test_foo.py:FooTest.test_bar"
-        expected_command = ("SCREENSHOT_DIR='{repo_dir}/test_root/log' "
-                                       "HAR_DIR='{repo_dir}/test_root/log/hars' "
-                                       "SELENIUM_DRIVER_LOG_DIR='{repo_dir}/test_root/log' nosetests "
-                                       "{repo_dir}/common/test/acceptance/tests/test_foo.py:FooTest.test_bar "
-                                       "--with-xunit "
-                                       "--xunit-file={repo_dir}/reports/bok_choy/xunit.xml "
-                                       "--verbosity=2 ".format(repo_dir=REPO_DIR))
-        self.assertEqual(request.cmd, expected_command)
+        self.request.test_spec = "test_foo.py:FooTest.test_bar"
+        self.assertEqual(self.request.cmd, self._expected_command(self.request.test_spec))
+
 
     # TODO: Test when bok_choy test file is in a subdir
